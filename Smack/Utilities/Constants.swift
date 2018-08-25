cf88d9bd35413f8b66a961b5e72baec11dc2f40e
1aaron <aaronnek1@gmail.com>
2018-08-25 14:35:07 -0500

    added service alamofire

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -7,7 +7,19 @@
 //
 
 import Foundation
+
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
 //Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+//USER DEFAULTS
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+//URLS
+let BASE_URL = "https://mychatjb.herokuapp.com/V1/"
+let REGISTER_ENDPOINT = "\(BASE_URL)account/register"
